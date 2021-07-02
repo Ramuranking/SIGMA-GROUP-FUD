9d884d45c93091c468990d400f26e3cd0db6fa1d
Fcp/ccs/18/1009(Aliyu Umar Umar) <83681900+Ramuranking@users.noreply.github.com>
2021-07-02 23:06:52 +0100

    Update SIGMA FORM.php

--- SIGMA FORM.php
+++ SIGMA FORM.php
@@ -1,30 +1,48 @@
 <?php
-// database connection code
-if(isset($_POST['txtName']))
-{
-// $con = mysqli_connect('localhost', 'database_user', 'database_password','database');
-$con = mysqli_connect('localhost', 'root', '','db_contact');
+require_once 'connectiondb.php';
 
-// get the post records
-
-$txtName = $_POST['txtName'];
-$txtEmail = $_POST['txtEmail'];
-$txtPhone = $_POST['txtPhone'];
-$txtMessage = $_POST['txtMessage'];
-
-// database insert SQL code
-$sql = "INSERT INTO `tbl_contact` (`Id`, `fldName`, `fldEmail`, `fldPhone`, `fldMessage`) VALUES ('0', '$txtName', '$txtEmail', '$txtPhone', '$txtMessage')";
-
-// insert in database 
-$rs = mysqli_query($con, $sql);
-if($rs)
-{
-	echo "Form Records Inserted";
-}
-}
-else
-{
-	echo "You are not a genuine visitor?";
-
-}
+// Get all departments by name
+$query = 'SELECT * FROM department';
+$statement=$db->prepare($query);
+$statement->execute();
+$departments=$statement->fetchAll();
+$statement->closeCursor();
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add Student</title>
+</head>
+<body>
+    <h1>Add New Student</h1>
+    <form action="add_student.php" method="POST">
+      <label for="firstname">First Name:</label>
+      <input type="text" name="firstname"><br>
+
+      <label for="surname">Last Name:</label>
+      <input type="text" name="surname"><br>
+
+       <label for="gender">Gender</label>
+       <input type="radio" name="gender" value="male">Male &nbsp;
+       <input type="radio" name="gender" value="female">Female<br>
+    
+       <label for="dob">Birth date:</label>
+       <input type="date" name="dob"><br>
+
+      <label for="dept">Department</label>
+      <select name="dept">
+        <?php foreach($departments as $dept): ?>
+        <option value="<?php echo $dept['id']; ?>">
+            <?php echo $dept['name']; ?>
+        </option>
+         <?php endforeach; ?>
+      </select><br>
+
+      <label for="submit">&nbsp;</label>
+      <input type="submit" name="submit" value="Add">
+    </form>
+</body>
+</html>
