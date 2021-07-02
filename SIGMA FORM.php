3b068eb5b33db3af390291cb4ba80f1e9504db55
Fcp/ccs/18/1009(Aliyu Umar Umar) <83681900+Ramuranking@users.noreply.github.com>
2021-07-02 22:23:30 +0100

    Update SIGMA FORM.php

--- SIGMA FORM.php
+++ SIGMA FORM.php
@@ -1,2 +1,30 @@
+<?php
+// database connection code
+if(isset($_POST['txtName']))
+{
+// $con = mysqli_connect('localhost', 'database_user', 'database_password','database');
+$con = mysqli_connect('localhost', 'root', '','db_contact');
 
-dgffhfhgf
+// get the post records
+
+$txtName = $_POST['txtName'];
+$txtEmail = $_POST['txtEmail'];
+$txtPhone = $_POST['txtPhone'];
+$txtMessage = $_POST['txtMessage'];
+
+// database insert SQL code
+$sql = "INSERT INTO `tbl_contact` (`Id`, `fldName`, `fldEmail`, `fldPhone`, `fldMessage`) VALUES ('0', '$txtName', '$txtEmail', '$txtPhone', '$txtMessage')";
+
+// insert in database 
+$rs = mysqli_query($con, $sql);
+if($rs)
+{
+	echo "Form Records Inserted";
+}
+}
+else
+{
+	echo "You are not a genuine visitor?";
+
+}
+?>
