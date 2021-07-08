fb059638c50e34ceda901e5b3159dd22fb689706
Rahimat Ohunene Taiwo <85590818+Ranny2020@users.noreply.github.com>
2021-07-08 13:52:12 +0100

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,8 +1,9 @@
 <?php
 if(isset($_POST['submit'])){
     $id = filter_input(INPUT_POST, 'id', FILTER_VALIDATE_INT);
-    $dept = filter_input(INPUT_POST, 'dept', FILTER_VALIDATE_INT);
-
+    $txtEmail = filter_input(INPUT_POST, 'txtEmail', FILTER_VALIDATE_INT);
+    $txtphone = filter_input(INPUT_POST, 'txtPhone', FILTER_VALIDATE_INT);
+    
    if($id === false || $id === NULL || $txtEmail === NULL || $txtEmail === false || $txtPhone ==false || $txtPhone == NULL) {
        echo 'Invalid stud_id or dept_id';
        exit();
@@ -12,7 +13,8 @@ if(isset($_POST['submit'])){
        $query = 'DELETE FROM formcontact WHERE id =:id AND txtEmail =:txtEmai AND txtPhone =:txtphone';
        $statement=$db->prepare($query);
        $statement->bindValue(':id', $id);
-       $statement->bindValue(':dept', $dept);
+       $statement->bindValue(':txtEmail', $txtEmail);
+       $statement->bindValue(':txtPhone', $txtphone);
        $statement->execute();
        $statement->closeCursor();
        header('Location: index.php');
